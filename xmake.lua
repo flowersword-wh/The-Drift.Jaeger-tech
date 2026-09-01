8fd03911b47131e7ca29d33bcea3a3fd5f1e5d1c
AuroBreeze <1732373074@qq.com>
2026-09-01 11:16:28 +0800

    build(xmake): migrate project build configuration
    - configure C++17 and UTF-8 compiler flags
    - add server, client, and file overview targets
    - ignore generated xmake files
    - remove obsolete CMake configuration

---
+++ xmake.lua
@@ -0,0 +1,94 @@
+set_project("The-Drift.Jaeger-tech")
+set_version("0.0.1")
+-- 设置最小版本为：2.1.0，低于此版本的xmake编译此工程将会提示版本错误信息
+set_xmakever("3.1.1")
+
+add_rules("mode.debug", "mode.release")
+
+-- std::string_view requires C++17.  /utf-8 also makes MSVC parse the
+-- Chinese comments and other UTF-8 source text without using code page 936.
+set_languages("c++17")
+add_cxflags("/utf-8")
+
+add_includedirs("include")
+
+target("server")
+    set_kind("binary")
+    add_files("server.cpp", "fileoverview.cpp")
+
+target("client")
+    set_kind("binary")
+    add_files("client.cpp")
+
+target("fileoverview")
+    set_kind("binary")
+    add_files("fileoverview_main.cpp", "fileoverview.cpp")
+
+--
+-- If you want to known more usage about xmake, please see https://xmake.io
+--
+-- ## FAQ
+--
+-- You can enter the project directory firstly before building project.
+--
+--   $ cd projectdir
+--
+-- 1. How to build project?
+--
+--   $ xmake
+--
+-- 2. How to configure project?
+--
+--   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
+--
+-- 3. Where is the build output directory?
+--
+--   The default output directory is `./build` and you can configure the output directory.
+--
+--   $ xmake f -o outputdir
+--   $ xmake
+--
+-- 4. How to run and debug target after building project?
+--
+--   $ xmake run [targetname]
+--   $ xmake run -d [targetname]
+--
+-- 5. How to install target to the system directory or other output directory?
+--
+--   $ xmake install
+--   $ xmake install -o installdir
+--
+-- 6. Add some frequently-used compilation flags in xmake.lua
+--
+-- @code
+--    -- add debug and release modes
+--    add_rules("mode.debug", "mode.release")
+--
+--    -- add macro definition
+--    add_defines("NDEBUG", "_GNU_SOURCE=1")
+--
+--    -- set warning all as error
+--    set_warnings("all", "error")
+--
+--    -- set language: c99, c++11
+--    set_languages("c99", "c++11")
+--
+--    -- set optimization: none, faster, fastest, smallest
+--    set_optimize("fastest")
+--
+--    -- add include search directories
+--    add_includedirs("/usr/include", "/usr/local/include")
+--
+--    -- add link libraries and search directories
+--    add_links("tbox")
+--    add_linkdirs("/usr/local/lib", "/usr/lib")
+--
+--    -- add system link libraries
+--    add_syslinks("z", "pthread")
+--
+--    -- add compilation and link flags
+--    add_cxflags("-stdnolib", "-fno-strict-aliasing")
+--    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
+--
+-- @endcode
+--
